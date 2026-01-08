c475f90c361eca41654233373af4b62d3aac6281
Luca Rossi <lucaerossi2gmail.com>
2026-01-08 04:08:17 -0300

    Seccion mas recientes

--- js/guide.js
+++ js/guide.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const popular = document.getElementById("popular");
+
+    function filtrarPorFecha() {
+        popular.innerHTML = ""; // Limpia el contenedor antes de agregar nuevas guías
+
+        const filtroFecha = [...guides].sort(
+            (a,b) => Number(b.dataFecha) - Number(a.dataFecha)
+        ); // Ordena las guías por fecha descendente
+
+        const fragmento = document.createDocumentFragment();
+
+        for (let i = 0; i < 3; i++) {
+            fragmento.appendChild(crearGuiaPopular(filtroFecha[i], i));
+        }
+
+        popular.appendChild(fragmento);
+    }
+
+    filtrarPorFecha();
+});
+
 const guideContainer = document.getElementById("contenedor");
 
 class guide {
@@ -11,18 +33,18 @@ class guide {
 };
 
 const guides = [
-    new guide("ps2", "", "../assets/portadas/kh1ps2.jpg", "Kingdom Hearts"),
-    new guide("ps2", "", "../assets/portadas/recodeveronicax.jpg", "Resident Evil: Code Veronica X", "../html/guias/recodeveronica.html"),
-    new guide("ps3", "", "../assets/portadas/ds2.jpg", "Dark Souls 2: Scholar of the First Sin"),
-    new guide("ps2", "", "../assets/portadas/crash-twinsanity.jpg", "Crash Twinsanity"),
-    new guide("ps2", "", "../assets/portadas/dbz-bt-3.jpg", "DBZ: Budokai Tenkaichi 3"),
-    new guide("ps2", "", "../assets/portadas/dmc.jpg", "Devil May Cry"),
-    new guide("ps2", "", "../assets/portadas/god-of-war.jpg", "God of War"),
-    new guide("ps2", "", "../assets/portadas/jak-daxter.jpg", "Jak and Daxter"),
-    new guide("ps2", "", "../assets/portadas/kh2fmps2.jpg", "Kingdom Hearts 2: Final Mix"),
-    new guide("ps2", "", "../assets/portadas/kh-com-ps2.jpg", "Kingdom Hearts Chain of Memories"),
-    new guide("ps2", "", "../assets/portadas/persona3.jpg", "Persona 3"),
-    new guide("ps2", "", "../assets/portadas/hl2.jpg", "Half Life 2"),
+    new guide("ps2", "26101", "../assets/portadas/kh1ps2.jpg", "Kingdom Hearts"),
+    new guide("ps2", "26102", "../assets/portadas/recodeveronicax.jpg", "Resident Evil: Code Veronica X", "../html/guias/recodeveronica.html"),
+    new guide("ps3", "26103", "../assets/portadas/ds2.jpg", "Dark Souls 2: Scholar of the First Sin"),
+    new guide("ps2", "26104", "../assets/portadas/crash-twinsanity.jpg", "Crash Twinsanity"),
+    new guide("ps2", "26105", "../assets/portadas/dbz-bt-3.jpg", "DBZ: Budokai Tenkaichi 3"),
+    new guide("ps2", "26106", "../assets/portadas/dmc.jpg", "Devil May Cry"),
+    new guide("ps2", "26107", "../assets/portadas/god-of-war.jpg", "God of War"),
+    new guide("ps2", "26108", "../assets/portadas/jak-daxter.jpg", "Jak and Daxter"),
+    new guide("ps2", "26109", "../assets/portadas/kh2fmps2.jpg", "Kingdom Hearts 2: Final Mix"),
+    new guide("ps2", "26110", "../assets/portadas/kh-com-ps2.jpg", "Kingdom Hearts Chain of Memories"),
+    new guide("ps2", "26111", "../assets/portadas/persona3.jpg", "Persona 3"),
+    new guide("ps2", "26112", "../assets/portadas/hl2.jpg", "Half Life 2"),
 ];
 
 
@@ -95,3 +117,13 @@ btnExpandir.addEventListener("click", () => {
     }
 });
 
+function crearGuiaPopular(guia, index = 0) {
+    let div = document.createElement("div");
+    div.classList.add("guide-card-popular", "escala");
+    div.dataset.consola = guia.dataConsola;
+    div.dataset.fecha = guia.dataFecha || "";
+    div.tabIndex = index + 1;
+
+    div.innerHTML = crearCardGuia(guia.img, guia.titulo, guia.link);
+    return div;
+}
